65b3eda77fb52e14d5cdab4386d159aabbc5c518
alvarezalbertocarlos <141088390+alvarezalbertocarlos@users.noreply.github.com>
2023-08-06 00:47:29 -0500

    Update db.py
    adding the db module where we test the connection to my using the mysql.connector

--- python-mysql/db.py
+++ python-mysql/db.py
@@ -1,1 +1,16 @@
+import mysql.connector
 
+db = mysql.connector.connect(
+    host='localhost',
+    user='USER_NAME',
+    password='USER_PASSWORD',
+    database='DB_NAME'
+)
+
+c = db.cursor()
+
+c.execute("select * from prueba.Usuario")
+
+res = c.fetchall()
+
+print(res)
